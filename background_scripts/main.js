e00812b89039314d2b218d5853790bd9f06af803
Kenny Chang <kennychang@zoho.com>
2022-02-21 12:35:35 -0500

    update .zip

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -37,18 +37,4 @@ function convert(t, s) {
         const mm = new MessageManager(port);
         mm.registerHandler('convert', s => convert(t, s));
     });
-
-    /* Context Menu */
-
-    browser.contextMenus.onClicked.addListener((info, tab) => {
-        if (info.menuItemId === 'do-inject-jyutping') {
-            browser.tabs.sendMessage(tab.id, { name: 'do-inject-jyutping' });
-        }
-    });
-
-    browser.contextMenus.create({
-        id: 'do-inject-jyutping',
-        title: browser.i18n.getMessage('contextMenuItemDoInjectJyutping'),
-        contexts: ['page'],
-    });
 })();
